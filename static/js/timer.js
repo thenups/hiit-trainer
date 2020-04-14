cc0f9aca0615b9a0cacaa08d11d94c5bb9c49478
TheNups <nn.mathur@gmail.com>
2020-04-13 22:11:21 -0700

    fix dual timer sync

--- static/js/timer.js
+++ static/js/timer.js
@@ -1,49 +1,30 @@
 /* Timer based on: http://forum.codecall.net/topic/51639-how-to-create-a-countdown-timer-in-javascript/ */
 
 var $timerDiv = document.querySelector('#timer');
-var $totalTimerDiv = document.querySelector('#timer'); //selected div to put the timer in
+var $totalTimerDiv = document.querySelector('#totalTimer');
+var $exerciseNameDiv = document.querySelector('#exerciseName');
 
 // userInput's about how much time they need
-var details = [
+var workout = [
   {
     exercise : 'something1',
-    time : 10
+    time : 8
   },
   {
     exercise : 'something2',
-    time : 25
+    time : 6
   },
   {
     exercise : 'rest',
-    time : 5
+    time : 3
   }
 ]
 
-loadTimers(details);
+setupTimer(workout);
 
-function loadTimers(workout) {
-  var $timerDiv = document.querySelector('#timer');
-  var $totalTimerDiv = document.querySelector('#totalTimer');
-  var $exerciseNameDiv = document.querySelector('#exerciseName');
-
-  nextExercise(workout, $timerDiv);
-
-  setupTimer($totalTimerDiv, findWorkoutTime(workout));
-
-}
-
-function nextExercise(workout, div){
-  setupTimer($timerDiv, workout[0]["time"]);
-
-  for (var i = 1; i < workout.length; i++) {
-    console.log(i);
-    var exerciseTime = workout[i]["time"];
-    setInterval(function(){setupTimer($timerDiv, exerciseTime);}, 999*exerciseTime);
-  }
-}
 
 // Go through exercise dict and deterime total time for workout
-function findWorkoutTime(exercises) {
+function totalTime(exercises) {
   var totalTime = 0;
 
   for (var i = 0; i < exercises.length; i++) {
@@ -54,23 +35,47 @@ function findWorkoutTime(exercises) {
 }
 
 // Put starting time on clock and start 1 second countdown
-function setupTimer(div, t) {
-  updateTimer(div, t);
-  window.setTimeout(countdown, 1000, div, t); //run countdown every 1 second
+function setupTimer(w) {
+  updateTimer($timerDiv, w[0]["time"]); //setup first exercise
+  updateTimer($totalTimerDiv, totalTime(w)); //setup total timeout
+
+  // Insert exercise name on screen
+  $exerciseNameDiv.innerHTML = w[0]["exercise"];
+  window.setTimeout(countdown, 1000, w); //run countdown every 1 second
 }
 
 // Stop timer if timer = 0, subtract 1 every second)
-function countdown(div, t) {
+function countdown(w) {
 
-  if (t <= 1) {
-    console.log("Time's up!")
+  // Subtract 1 from most recent exercise
+  for (var i = 0; i < w.length; i++) {
+    console.log('-------------');
+
+    if (w[i]["time"] > 0) { // check to see if the time is more than one for that list item
+      w[i]["time"] = w[i]["time"]-1;
+
+      // Update Exercise Timer
+      updateTimer($timerDiv, w[i]["time"]);
+      // Update Exercise Name
+      $exerciseNameDiv.innerHTML = w[i]["exercise"];
+
+
+      break;
+    }
+
+  }
+
+  // Update total timer
+  updateTimer($totalTimerDiv, totalTime(w));
+
+  console.log(totalTime(w));
+
+  if (totalTime(w) <= 0) {
+    console.log("Time's up!");
     return;
   }
 
-  t -= 1;
-  updateTimer(div, t)
-
-  window.setTimeout(countdown, 1000, div, t);
+  window.setTimeout(countdown, 1000, w);
 }
 
 // Figure out minutes and seconds to write out clock
